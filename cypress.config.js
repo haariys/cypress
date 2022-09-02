32edf2213d9b782b0d3dc4cd6ec59ba36157e66e
haris2057 <muhammad.haris@carepvtltd.com>
2022-09-02 17:05:25 +0500

    config update

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,5 @@
 const { defineConfig } = require('cypress');
 const cucumber = require('cypress-cucumber-preprocessor').default
-
-
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
@@ -14,26 +12,21 @@ module.exports = defineConfig({
     json: true,
   },
   e2e: {
-
     specPattern: ["**/*.feature", "**/*.cy.{js,jsx,ts,tsx}"],
     baseUrl: 'http://172.16.10.4:8082/jw/web/login',
     setupNodeEvents(on, config) {
       on('file:preprocessor', cucumber())
       require('cypress-mochawesome-reporter/plugin')(on);
-
-
     },
-    
     env: {
       google: 'https://www.google.com.pk',
       user_id: 'admin',
       user_pass: 'Tomcat@123',
       email_regex: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
-
     },
     trashAssetsBeforeRuns: false,
     //experimentalSessionAndOrigin : true,
-    // includeShadowDom: true,
+    //includeShadowDom: true,
     chromeWebSecurity: false
   },
 });
